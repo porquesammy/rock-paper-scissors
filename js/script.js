3ab73b89eb744024cd66ab662df9b5034ad9472c
porqueSammy <42855916+porquesammy@users.noreply.github.com>
2023-02-25 15:38:22 -0800

    fix logic and scope, give game structure, add score function

--- js/script.js
+++ js/script.js
@@ -1,79 +1,101 @@
 "use strict";
 
-// computer choice rock = 1, paper = 2, scissors = 3
+let computerSelection;
 
-let computerChoice = Math.floor(Math.random() * 3) + 1;
-// console.log(computerChoice);
-
-//convert computers choice into rock, paper, scissors 
-switch (computerChoice) {
-    case 1:
-        computerChoice = 'rock';
-        break;
-    case 2:
-        computerChoice = 'paper';
-        break;
-    case 3:
-        computerChoice = 'scissors';
-        break;
-    default:
-        console.warn("Something went horribly wrong!!!")
-}
-// console.log(computerChoice);
-
-
-//player choice prompted, converted to lowercase, and checked
-
-let playerChoice
-
-function checkChoice() {
-    playerChoice = prompt('Please Choose: Rock, Paper, or Scissors');
-    playerChoice = playerChoice.toLowerCase();
-    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-        return playerChoice;
-    } else {
-        checkChoice();
+function getComputerChoice() {
+    computerSelection = Math.floor(Math.random() * 3) + 1;
+    switch (computerSelection) {
+        case 1:
+            computerSelection = 'rock';
+            break;
+        case 2:
+            computerSelection = 'paper';
+            break;
+        case 3:
+            computerSelection = 'scissors';
+            break;
+        default:
+            console.warn("Something went horribly wrong!!!")
     }
 }
-checkChoice();
 
-// compare player choice vs computers choice and declare the results
-console.log('You chose' + ' ', playerChoice);
-console.log('Computer chose' + ' ', computerChoice);
+let playerSelection
 
-switch (true) {
-
-    case (playerChoice === 'rock'):
-        if (computerChoice === 'rock') {
-            console.log('You tie!');
-        } else if (computerChoice === 'paper') {
-            console.log('You lose!');
-        } else {
-            console.log('You Win!');
-        }
-        break;
-
-    case (playerChoice === 'paper'):
-        if (computerChoice === 'paper') {
-            console.log('You tie!');
-        } else if (computerChoice === 'scissors') {
-            console.log('You lose!');
-        } else {
-            console.log('You Win!');
-        }
-        break;
-
-    case (playerChoice === 'scissors'):
-        if (computerChoice === 'scissors') {
-            console.log('You tie!');
-        } else if (computerChoice === 'rock') {
-            console.log('You lose!');
-        } else {
-            console.log('You Win!');
-        }
-        break;
-    default:
-        console.warn('something went wrong with my switch statement')
+function getPlayerChoice() {
+    playerSelection = prompt('Please Choose: Rock, Paper, or Scissors');
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+        return playerSelection;
+    } else {
+        getPlayerChoice();
+    }
 }
 
 
+let result;
+
+function checkResult() {
+
+    switch (true) {
+
+        case (playerSelection === 'rock'):
+            if (computerSelection === 'rock') {
+                result = 'tie';
+            } else if (computerSelection === 'paper') {
+                result = 'lose';
+            } else {
+                result = 'win';
+            }
+            break;
+
+        case (playerSelection === 'paper'):
+            if (computerSelection === 'paper') {
+                result = 'tie';
+            } else if (computerSelection === 'scissors') {
+                result = 'lose';
+            } else {
+                result = 'win';
+            }
+            break;
+
+        case (playerSelection === 'scissors'):
+            if (computerSelection === 'scissors') {
+                result = 'tie';
+            } else if (computerSelection === 'rock') {
+                result = 'lose';
+            } else {
+                result = 'win';
+            }
+            break;
+        default:
+            console.warn('something went wrong');
+    }
+}
+
+
+//game structure calling functions and updating score
+function startGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        getComputerChoice();
+        getPlayerChoice();
+        checkResult();
+
+        if (result === 'win') {
+            ++playerScore;
+        } else if (result === 'lose') {
+            ++computerScore;
+        }
+        console.log(`You chose ${playerSelection}`);
+        console.log(`The computer chose ${computerSelection}`);
+        console.log(`You ${result}!`);
+        console.log(`Current Score: 
+        You: ${playerScore} Computer: ${computerScore} 
+        `);
+    }
+    console.log('GAME OVER!');
+}
+
+console.log('Type "startGame()" to begin');
