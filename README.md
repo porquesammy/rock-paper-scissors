9695a12a1c28222c759fa330d0cb37344406575b
porqueSammy <42855916+porquesammy@users.noreply.github.com>
2023-03-15 11:25:16 -0700

    Chore: Incapsulate variables and functions so they are not available in the console. Reformat JS for readability.  Add task to README.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rock-paper-scissors
+
 first Odin javaScript project from scratch
+
+to-do:
+
+-Take feedback off svg/buttons when game is not in playable state
+-Change svg images to buttons/ give aria tags
+-Announce results of game
+-Add games won
+-Animate transitions
+-Add prompt style trash-talk json for robot
--- js/script.js
+++ js/script.js
@@ -1,192 +1,199 @@
-'use strict';
+"use strict";
+// put whole game into a function to stop console changes to game state
+function wholeGame() {
+  let computerSelection;
+  let playerSelection;
+  let result;
+  let playerScore = 0;
+  let computerScore = 0;
+  let totalGamesWon = 0;
+  let totalGamesLost = 0;
 
-let computerSelection;
-let playerSelection;
-let result;
-let playerScore = 0;
-let computerScore = 0;
-let totalGamesWon = 0;
-let totalGamesLost = 0;
+  const playerScoreOne = document.querySelector(".playerScoreOne-btn");
+  const playerScoreTwo = document.querySelector(".playerScoreTwo-btn");
+  const playerScoreThree = document.querySelector(".playerScoreThree-btn");
 
-const playerScoreOne = document.querySelector('.playerScoreOne-btn');
-const playerScoreTwo = document.querySelector('.playerScoreTwo-btn');
-const playerScoreThree = document.querySelector('.playerScoreThree-btn');
+  const cpuScoreOne = document.querySelector(".cpuScoreOne-btn");
+  const cpuScoreTwo = document.querySelector(".cpuScoreTwo-btn");
+  const cpuScoreThree = document.querySelector(".cpuScoreThree-btn");
 
-const cpuScoreOne = document.querySelector('.cpuScoreOne-btn');
-const cpuScoreTwo = document.querySelector('.cpuScoreTwo-btn');
-const cpuScoreThree = document.querySelector('.cpuScoreThree-btn');
+  const playerText = document.querySelector(".player-choice");
+  const computerText = document.querySelector(".robot-choice");
+  const choose = document.querySelector(".choose");
 
-const playerText = document.querySelector('.player-choice');
-const computerText = document.querySelector('.robot-choice');
-const choose = document.querySelector('.choose');
+  const readyText = document.querySelector(".ready");
 
-const readyText = document.querySelector('.ready');
+  const rock = document.getElementById("rock");
+  const paper = document.getElementById("paper");
+  const scissors = document.getElementById("scissors");
 
-document.getElementById('rock').onclick = function () {
-  playerSelection = 'rock';
-  game();
-};
+  rock.onclick = function () {
+    playerSelection = "rock";
+    game();
+  };
 
-document.getElementById('paper').onclick = function () {
-  playerSelection = 'paper';
-  game();
-};
+  paper.onclick = function () {
+    playerSelection = "paper";
+    game();
+  };
 
-document.getElementById('scissors').onclick = function () {
-  playerSelection = 'scissors';
-  game();
-};
+  scissors.onclick = function () {
+    playerSelection = "scissors";
+    game();
+  };
 
-function reset() {
-  playerScore = 0;
-  computerScore = 0;
-  clearAllLights();
-  if (document.getElementById('reset')){
-    document.getElementById('reset').removeEventListener('click', reset, false);
-};
-  choose.removeAttribute('id');
-  choose.innerText = 'Choose one!';
-  playerText.style.display = 'none';
-  computerText.style.display = 'none';
-  choose.removeAttribute('class', 'new-game');
-  readyText.style.display = 'block';
-}
+  function game() {
+    readyText.style.display = "none";
+    playerText.style.display = "block";
+    computerText.style.display = "block";
+    const playerTextSpan = document.querySelector(".player-choice>span");
+    const computerTextSpan = document.querySelector(".robot-choice>span");
 
-function clearAllLights() {
-  const lights = document.querySelectorAll('.score');
-  const lightsArr = Array.from(lights);
+    if (playerScore < 3 && computerScore < 3) {
+      getComputerChoice();
+      checkResult();
+      playerTextSpan.innerText = `${playerSelection}`;
+      computerTextSpan.innerText = `${computerSelection}`;
 
-  lightsArr.forEach((e) => {
-    if (e.classList.contains('win')) {
-      e.classList.remove('win');
-    } else {
-      return;
+      if (result === "win") {
+        ++playerScore;
+      } else if (result === "lose") {
+        ++computerScore;
+      } else {
+        result = "tie";
+      }
     }
-  });
-}
 
-function getComputerChoice() {
-  computerSelection = Math.floor(Math.random() * 3) + 1;
-  switch (computerSelection) {
-    case 1:
-      computerSelection = 'rock';
-      break;
-    case 2:
-      computerSelection = 'paper';
-      break;
-    case 3:
-      computerSelection = 'scissors';
-      break;
-    default:
-      console.warn('Something went horribly wrong!!!');
-  }
-}
-
-function checkResult() {
-  switch (playerSelection) {
-    case 'rock':
-      if (computerSelection === 'rock') {
-        result = 'tie';
-      } else if (computerSelection === 'paper') {
-        result = 'lose';
-      } else {
-        result = 'win';
+    function getComputerChoice() {
+      computerSelection = Math.floor(Math.random() * 3) + 1;
+      switch (computerSelection) {
+        case 1:
+          computerSelection = "rock";
+          break;
+        case 2:
+          computerSelection = "paper";
+          break;
+        case 3:
+          computerSelection = "scissors";
+          break;
+        default:
+          console.warn("Something went horribly wrong!!!");
       }
-      break;
-
-    case 'paper':
-      if (computerSelection === 'paper') {
-        result = 'tie';
-      } else if (computerSelection === 'scissors') {
-        result = 'lose';
-      } else {
-        result = 'win';
-      }
-      break;
-
-    case 'scissors':
-      if (computerSelection === 'scissors') {
-        result = 'tie';
-      } else if (computerSelection === 'rock') {
-        result = 'lose';
-      } else {
-        result = 'win';
-      }
-      break;
-    default:
-      console.warn('something went wrong');
-  }
-}
-
-function game() {
-    readyText.style.display = 'none';
-    playerText.style.display = 'block';
-    computerText.style.display = 'block';
-    const playerTextSpan = document.querySelector('.player-choice>span');
-    const computerTextSpan = document.querySelector('.robot-choice>span');
-
-  if (playerScore < 3 && computerScore < 3) {
-    getComputerChoice();
-    checkResult();
-    playerTextSpan.innerText = `${playerSelection}`;
-    computerTextSpan.innerText = `${computerSelection}`;
-
-    if (result === 'win') {
-      ++playerScore;
-
-      
-    } else if (result === 'lose') {
-      ++computerScore;
-
-    } else {
-      result = 'tie';
     }
-  }
 
-  //scoreboard lights
-  switch (playerScore) {
-    case 0:
-      break;
-    case 1:
-      playerScoreOne.classList.add('win');
-      break;
-    case 2:
-      playerScoreTwo.classList.add('win');
-      break;
-    case 3:
-      playerScoreThree.classList.add('win');
-      ++totalGamesWon;
-      playerScore = undefined;
-      computerScore = undefined;
-      choose.innerText = 'New Game?';
-      choose.setAttribute('id', 'reset');
-      choose.setAttribute('class', 'new-game');
-      break;
-  }
+    function checkResult() {
+      switch (playerSelection) {
+        case "rock":
+          if (computerSelection === "rock") {
+            result = "tie";
+          } else if (computerSelection === "paper") {
+            result = "lose";
+          } else {
+            result = "win";
+          }
+          break;
 
-  switch (computerScore) {
-    case 0:
-      break;
-    case 1:
-      cpuScoreOne.classList.add('win');
-      break;
-    case 2:
-      cpuScoreTwo.classList.add('win');
-      break;
-    case 3:
-      cpuScoreThree.classList.add('win');
-      ++totalGamesLost;
-      console.log('you lose');
-      playerScore = undefined;
-      computerScore = undefined;
-      choose.innerText = 'New Game?';
-      choose.setAttribute('id', 'reset');
-      choose.setAttribute('class', 'new-game');
-      break;
-  }
-  if (choose.hasAttribute('id', 'reset')) {
-    document.getElementById('reset').onclick = function () {
-      reset();
+        case "paper":
+          if (computerSelection === "paper") {
+            result = "tie";
+          } else if (computerSelection === "scissors") {
+            result = "lose";
+          } else {
+            result = "win";
+          }
+          break;
+
+        case "scissors":
+          if (computerSelection === "scissors") {
+            result = "tie";
+          } else if (computerSelection === "rock") {
+            result = "lose";
+          } else {
+            result = "win";
+          }
+          break;
+        default:
+          console.warn("something went wrong");
+      }
+    }
+
+    function reset() {
+      playerScore = 0;
+      computerScore = 0;
+      clearAllLights();
+      if (document.getElementById("reset")) {
+        document
+          .getElementById("reset")
+          .removeEventListener("click", reset, false);
+      }
+      choose.removeAttribute("id");
+      choose.innerText = "Choose one!";
+      playerText.style.display = "none";
+      computerText.style.display = "none";
+      choose.removeAttribute("class", "new-game");
+      readyText.style.display = "block";
+    }
+
+    function clearAllLights() {
+      const lights = document.querySelectorAll(".score");
+      const lightsArr = Array.from(lights);
+
+      lightsArr.forEach((e) => {
+        if (e.classList.contains("win")) {
+          e.classList.remove("win");
+        } else {
+          return;
+        }
+      });
+    }
+
+    //scoreboard lights
+    switch (playerScore) {
+      case 0:
+        break;
+      case 1:
+        playerScoreOne.classList.add("win");
+        break;
+      case 2:
+        playerScoreTwo.classList.add("win");
+        break;
+      case 3:
+        playerScoreThree.classList.add("win");
+        ++totalGamesWon;
+        playerScore = undefined;
+        computerScore = undefined;
+        choose.innerText = "New Game?";
+        choose.setAttribute("id", "reset");
+        choose.setAttribute("class", "new-game");
+        break;
+    }
+
+    switch (computerScore) {
+      case 0:
+        break;
+      case 1:
+        cpuScoreOne.classList.add("win");
+        break;
+      case 2:
+        cpuScoreTwo.classList.add("win");
+        break;
+      case 3:
+        cpuScoreThree.classList.add("win");
+        ++totalGamesLost;
+        console.log("you lose");
+        playerScore = undefined;
+        computerScore = undefined;
+        choose.innerText = "New Game?";
+        choose.setAttribute("id", "reset");
+        choose.setAttribute("class", "new-game");
+        break;
+    }
+    if (choose.hasAttribute("id", "reset")) {
+      document.getElementById("reset").onclick = function () {
+        reset();
+      };
     };
-  }
-}
+  };
+};
+
+wholeGame();
